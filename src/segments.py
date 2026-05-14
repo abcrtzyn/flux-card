496f5c3bdc6c1fc0644a100498cc7ff90ae9c827
Aidan Christensen <aidan_christensen@icloud.com>
2026-05-14 11:03:53 -0500

    segments is a dataclass

--- src/segments.py
+++ src/segments.py
@@ -1,18 +1,13 @@
 
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 
-
+@dataclass
 class Segment:
     job: str;
     inTime: datetime;
     outTime: datetime;
     description: str;
-
-    def __init__(self, job: str, inTime: datetime, outTime: datetime, description: str):
-        self.job = job;
-        self.inTime = inTime;
-        self.outTime = outTime;
-        self.description = description;
     
     def elapsed(self) -> timedelta:
         return self.outTime-self.inTime;
